f3946bc948010c9e7a89d925273567bc8008af6b
Ege Bayrak <72911001+egebayrak@users.noreply.github.com>
2023-03-31 15:39:20 +0300

    Add files via upload

---
+++ BinaryDecimal/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace BinaryDecimal
+{
+    partial class BinaryDecimalConverter
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(12, 9);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(344, 23);
+            this.txtInput.TabIndex = 0;
+            this.txtInput.TextChanged += new System.EventHandler(this.txtInput_TextChanged);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(93, 71);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 15);
+            this.lblResult.TabIndex = 1;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(12, 67);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(75, 23);
+            this.btnConvert.TabIndex = 2;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 38);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(344, 23);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Location = new System.Drawing.Point(281, 67);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(75, 23);
+            this.btnCopy.TabIndex = 4;
+            this.btnCopy.Text = "Copy";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // BinaryDecimalConverter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(372, 99);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.txtInput);
+            this.Name = "BinaryDecimalConverter";
+            this.Text = "Binary Decimal Converter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnCopy;
+    }
+}
+
---
+++ BinaryDecimal/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BinaryDecimal
+{
+    public partial class BinaryDecimalConverter : Form
+    {
+        public BinaryDecimalConverter()
+        {
+            InitializeComponent();
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox1.Items.AddRange(new object[] { "Binary to Decimal", "Decimal to Binary" });
+        }
+
+        public int ConvertToDecimal(string binary)
+        {
+            int result = 0;
+            int pow = 1;
+
+            for (int i = binary.Length - 1; i >= 0; i--)
+            {
+                int digit = int.Parse(binary[i].ToString());
+                result += pow * digit;
+                pow *= 2;
+            }
+
+            return result;
+        }
+
+        public string ConvertToBinary(int decimalNumber)
+        {
+            string binary = "";
+
+            while (decimalNumber > 0)
+            {
+                int remainder = decimalNumber % 2;
+                decimalNumber /= 2;
+                binary = remainder.ToString() + binary;
+            }
+
+            return binary;
+        }
+
+        private void txtInput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(lblResult.Text);
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(txtInput.Text, out int number))
+            {
+                MessageBox.Show("Lütfen geçerli bir sayı girin.");
+                txtInput.Clear();
+                return;
+            }
+
+            string input = txtInput.Text.ToString();
+            string selected = comboBox1.SelectedItem.ToString();
+
+            if (selected == "Binary to Decimal")
+            {
+                int result = ConvertToDecimal(input);
+                lblResult.Text = $"{result.ToString()}";
+            }
+            else if (selected == "Decimal to Binary")
+            {
+                string result = ConvertToBinary(number);
+                lblResult.Text = $"{result}";
+            }
+        }
+    }
+}
